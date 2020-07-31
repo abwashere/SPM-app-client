b31daad6f27e0736ab740b3d0755c1cf91e5dfac
Aude Richon <auderichon@gmail.com>
2020-07-31 17:25:47 +0200

    club profile without CSS

--- src/pages/profiles/Club.jsx
+++ src/pages/profiles/Club.jsx
@@ -1,11 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
+import clubApiHandler from "./../../api/clubApiHandler";
 
-const Club = (props) => {
-	return (
-		<div>
-			<h1>Club Profile Page ∆</h1>
-		</div>
-	);
-};
+class Club extends Component {
+  state = {
+    club: null,
+  };
+
+  componentDidMount() {
+    const clubId = this.props.match.params.id;
+
+    clubApiHandler
+      .getOneClub(clubId)
+      .then((apiRes) => {
+        console.log(apiRes);
+        this.setState({ club: apiRes });
+      })
+      .catch((apiError) => console.log(apiError));
+  }
+
+  render() {
+    if (!this.state.club) return null;
+
+    return (
+      <div>
+        <h1>{this.state.club.clubName}</h1>
+
+        <img src={this.state.club.image} alt={this.state.club.clubName} />
+        <br />
+        <h3>Nous joindre :</h3>
+        {!this.state.club.location.formattedAddress && this.state.club.address && (
+          <p>
+            <span className="bold">Adresse : </span>
+            {this.state.club.address}
+          </p>
+        )}
+        {this.state.club.location.formattedAddress && (
+          <p>
+            <span className="bold">Adresse : </span>
+            {this.state.club.location.formattedAddress}
+          </p>
+        )}
+        {this.state.club.phoneNumber && (
+          <p>
+            <span className="bold">Téléphone : </span>
+            {this.state.club.phoneNumber}
+          </p>
+        )}
+        <p>
+          <span className="bold">Email : </span>
+          {this.state.club.email}
+        </p>
+        <br />
+        {this.state.club.website && (
+          <p>
+            <span className="bold">Site web : </span>
+            {this.state.club.website}
+          </p>
+        )}
+        {this.state.club.videoURL && (
+          <p>
+            <span className="bold">Vidéo : </span>
+            {this.state.club.videoURL}
+          </p>
+        )}
+        <br />
+        {this.state.club.description && (
+          <p>
+            <span className="bold">Plus d'infos : </span>
+            {this.state.club.description}
+          </p>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Club;

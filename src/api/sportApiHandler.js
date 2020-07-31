baa447b9e31ed0543402cb6e81ebfd32219f9892
Aude Richon <auderichon@gmail.com>
2020-07-31 10:04:31 +0200

    création de l'apihandler sport

---
+++ src/api/sportApiHandler.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const service = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_URL + "/api/sport",
+  withCredentials: true, // Cookie is sent to client when using this service. (used for session)
+});
+
+function errorHandler(error) {
+  if (error.response.data) {
+    console.log(error.response && error.response.data);
+    throw error;
+  }
+  throw error;
+}
+
+export default {
+  service,
+
+  getSports() {
+    return service
+      .get("/")
+      .then((res) => res.data)
+      .catch(errorHandler);
+  },
+};

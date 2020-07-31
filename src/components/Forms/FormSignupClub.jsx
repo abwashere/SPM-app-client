debb0addcd6b21d85a64ba398cb2eef3ced74466
Aude Richon <auderichon@gmail.com>
2020-07-31 10:39:23 +0200

    update on club signup

--- src/components/Forms/FormSignupClub.jsx
+++ src/components/Forms/FormSignupClub.jsx
@@ -9,229 +9,6 @@ import "bulma/css/bulma.css";
 import "./../../styles/FormSignUp.css";
 
 class FormSignupClub extends Component {
-<<<<<<< HEAD
-	static contextType = UserContext;
-
-	state = {
-		role: "Club",
-		email: "",
-		password: "",
-		clubName: "",
-		address: "",
-		phoneNumber: "",
-		image: "",
-		tmpImage: "",
-		website: "",
-		foundedYear: "",
-		subscriptionFee: "",
-		description: "",
-	};
-
-	handleChange = (event) => {
-		const value =
-			event.target.type === "file"
-				? event.target.files[0]
-				: event.target.type === "checkbox"
-				? event.target.checked
-				: event.target.value;
-
-		const key = event.target.name;
-
-		this.setState({ [key]: value });
-		console.log("inputed : ", value);
-	};
-
-	handleSubmit = (event) => {
-		event.preventDefault();
-
-		const fd = new FormData();
-		for (let key in this.state) {
-			fd.append(key, this.state[key]);
-		}
-
-		authApiHandler
-			.signupClub(fd)
-			.then((data) => {
-				this.context.setUser(data);
-				this.props.history.push("/");
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
-	handleImage = (event) => {
-		this.setState({
-			avatar: event.target.files[0],
-			// URL temporaire qu'on peut inserer dans une balise <img  src={urlTemporaire}/>
-			// preview avant de soumettre le <form> au backend.
-			tmpAvatar: URL.createObjectURL(event.target.files[0]),
-		});
-	};
-
-	handlePlace = (place) => {
-		// This handle is passed as a callback to the autocomplete component.
-		console.log(place);
-	};
-
-	render() {
-		return (
-			<div className="FormSignup">
-				<form onChange={this.handleChange} onSubmit={this.handleSubmit}>
-					<div className="field">
-						<label className="label">Email</label>
-						<div className="control has-icons-left">
-							<input
-								className="input"
-								type="email"
-								name="email"
-								value="toto@foo.bar"
-							/>
-							<span className="icon is-small is-left">
-								<i className="fa fa-at"></i>
-							</span>
-						</div>
-					</div>
-					<div className="field">
-						<label htmlFor="password" className="label">
-							Choisir un mot de passe
-						</label>
-						<div className="control has-icons-left">
-							<input
-								className="input"
-								type="password"
-								placeholder="Entrez un mot de passe"
-								name="password"
-							/>
-							<span className="icon is-small is-left">
-								<i className="fa fa-lock"></i>
-							</span>
-						</div>
-					</div>
-					<div className="field">
-						<label className="label">Nom du club</label>
-						<div className="control">
-							<input
-								className="input"
-								type="text"
-								name="clubName"
-								defaultValue="Les TATAZ"
-							/>
-						</div>
-					</div>
-
-					<div className="field">
-						<label className="label">Adresse administrative</label>
-						<div className="control has-icons-left">
-							<LocationAutoComplete
-								onSelect={this.handlePlace}
-								searchIndication = "Entrez une adresse"
-							/>
-							<span className="icon is-small is-left">
-								<i className="fa fa-map-marker"></i>
-							</span>
-							<span className="icon is-small is-right">
-								{/* <i className="fas fa-check"></i> */}
-							</span>
-						</div>
-					</div>
-
-					<div className="field">
-						<label className="label">Téléphone</label>
-						<div className="control has-icons-left">
-							<input
-								className="input"
-								type="tel"
-								name="phoneNumber"
-								pattern="[0-9]{10}"
-								minLength="10"
-								maxLength="10"
-								defaultValue="0123456789"
-							/>
-							<span className="icon is-small is-left">
-								<i className="fa fa-phone"></i>
-							</span>
-						</div>
-					</div>
-
-					<div className="field">
-						<label className="label">Site web du club</label>
-						<div className="control">
-							<input
-								className="input"
-								type="text"
-								name="website"
-								defaultValue="tataz.com"
-							/>
-						</div>
-					</div>
-					<div className="field">
-						<label className="label">Année de création</label>
-						<div className="control">
-							<input
-								className="input"
-								type="text"
-								name="foundedYear"
-								defaultValue="2020"
-							/>
-						</div>
-					</div>
-					<div className="field">
-						<label className="label">Frais d'adhésion</label>
-						<div className="control">
-							<input
-								className="input"
-								type="text"
-								name="subscriptionFee"
-								defaultValue="100 ou 150€ annuel, 15€ mensuel"
-							/>
-						</div>
-					</div>
-
-					<div className="field">
-						<label className="label">Quelques précisions sur le club</label>
-						<div className="control">
-							<textarea
-								className="textarea"
-								name="description"
-								placeholder="Objectifs, ambitions, besoins, politique de recrutement... "
-								value="Faire la fête après les matchs !"
-							></textarea>
-						</div>
-					</div>
-
-					<label className="label">Télécharger une image</label>
-					<div class="file has-name">
-						<label class="file-label">
-							<input class="file-input" type="file" name="resume" />
-							<span class="file-cta">
-								<span class="file-icon">
-									<i class="fas fa-upload"></i>
-								</span>
-								<span class="file-label">Choose a file…</span>
-							</span>
-							<span class="file-name">{this.state.image}</span>
-						</label>
-					</div>
-
-					<div className="field btn-signup">
-						<div className="control">
-							<button className="button is-link">C'est fait !</button>
-						</div>
-					</div>
-				</form>
-
-				<div className="form-section-bottom">
-					<p>Vous avez déjà un compte ? </p>
-					<button class="button is-light">
-						<Link className="link" to="/signin">
-							Connectez-vous !
-						</Link>
-					</button>
-				</div>
-			</div>
-		);
-	}
-=======
   static contextType = UserContext;
 
   state = {
@@ -244,6 +21,7 @@ class FormSignupClub extends Component {
     image: "",
     tmpImage: "",
     website: "",
+    videoURL: "",
     year: "",
     subscriptionFee: "",
     description: "",
@@ -307,8 +85,7 @@ class FormSignupClub extends Component {
                 type="email"
                 name="email"
                 value="toto@foo.bar"
-                placeholder="Renseignez votre adresse mail"
-                required
+                placeholder="Choisissez le mail de l'association que vous consultez le plus régulièrement"
               />
               <span className="icon is-small is-left">
                 <i className="fa fa-at"></i>
@@ -325,7 +102,6 @@ class FormSignupClub extends Component {
                 type="password"
                 placeholder="Entrez un mot de passe"
                 name="password"
-                required
               />
               <span className="icon is-small is-left">
                 <i className="fa fa-lock"></i>
@@ -340,7 +116,6 @@ class FormSignupClub extends Component {
                 type="text"
                 name="clubName"
                 defaultValue="Les TATAZ"
-                required
               />
             </div>
           </div>
@@ -348,7 +123,10 @@ class FormSignupClub extends Component {
           <div className="field">
             <label className="label">Adresse administrative</label>
             <div className="control has-icons-left">
-              <LocationAutoComplete onSelect={this.handlePlace} />
+              <LocationAutoComplete
+                onSelect={this.handlePlace}
+                searchIndication="Renseignez l'adresse du club"
+              />
               <span className="icon is-small is-left">
                 <i className="fa fa-map-marker"></i>
               </span>
@@ -388,13 +166,13 @@ class FormSignupClub extends Component {
             </div>
           </div>
           <div className="field">
-            <label className="label">Vidéo de présentation du club</label>
+            <label className="label">Vidéo de présentation de votre club</label>
             <div className="control">
               <input
                 className="input"
                 type="text"
                 name="videoURL"
-                defaultValue=""
+                placeholder="Lien de la vidéo, si vous en avez une"
               />
             </div>
           </div>
@@ -416,8 +194,7 @@ class FormSignupClub extends Component {
                 className="input"
                 type="text"
                 name="subscriptionFee"
-                placeholder="Précisez les différentes modalités d'inscription"
-                defaultValue="100 ou 150€ annuel, 15€ mensuel"
+                defaultValue="Précisez les modalités d'inscription"
               />
             </div>
           </div>
@@ -428,7 +205,7 @@ class FormSignupClub extends Component {
               <textarea
                 className="textarea"
                 name="description"
-                placeholder="Objectifs, ambitions, besoins, politique de recrutement... tout ce que vous jugez utile de connaître pour un nouvel adhérent !"
+                placeholder="Objectifs, ambitions, besoins, politique de recrutement... "
                 value="Faire la fête après les matchs !"
               ></textarea>
             </div>
@@ -442,7 +219,7 @@ class FormSignupClub extends Component {
                 <span class="file-icon">
                   <i class="fas fa-upload"></i>
                 </span>
-                <span class="file-label">Choose a file…</span>
+                <span class="file-label">Choisir un fichier…</span>
               </span>
               <span class="file-name">{this.state.image}</span>
             </label>
@@ -466,7 +243,6 @@ class FormSignupClub extends Component {
       </div>
     );
   }
->>>>>>> update placeholders on signup club
 }
 
 export default withRouter(FormSignupClub);

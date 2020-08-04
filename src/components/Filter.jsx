88c09ce2c1f8c183a7c71b81c13788d03b7be450
Aude Richon <auderichon@gmail.com>
2020-08-04 20:19:09 +0200

    filtres sports OK et bouton effacer filtres qui ne foncitonne pas

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,99 +2,108 @@ import React from "react";
 import sportApiHandler from "./../api/sportApiHandler";
 
 class Filter extends React.Component {
-	state = {
-		onDisplay: null,
-		sportsList: null,
-		practice: null,
-		sport: null,
-		day: null,
-	};
+  state = {
+    onDisplay: null,
+    sportsList: null,
+    practice: null,
+    sport: null,
+    day: null,
+  };
 
-	componentDidMount() {
-		sportApiHandler
-			.getSports()
-			.then((apiRes) => {
-				this.setState({ sportsList: apiRes });
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	}
+  componentDidMount() {
+    sportApiHandler
+      .getSports()
+      .then((apiRes) => {
+        this.setState({ sportsList: apiRes });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-	displayFilters = () => {
-		this.state.onDisplay
-			? this.setState({ onDisplay: false })
-			: this.setState({ onDisplay: true });
-	};
+  displayFilters = () => {
+    this.state.onDisplay
+      ? this.setState({ onDisplay: false })
+      : this.setState({ onDisplay: true });
+  };
 
-	handleChange = (event) => {
-		const value = event.target.value;
-		const key = event.target.name;
-		this.setState({ [key]: value });
-	};
+  handleChange = (event) => {
+    const value = event.target.value;
+    const key = event.target.name;
+    this.setState({ [key]: value });
+  };
 
-	handleSubmit = (event) => {
-		event.preventDefault();
-		this.displayFilters();
-		let practices = this.state.practice;
-		let sports = this.state.sport;
-		let days = this.state.day;
-		let selection = [];
-		if (practices) selection.practice = practices;
-		if (sports) selection.sport = sports;
+  deleteFilters = (event) => {
+    console.log("effacer les filtres");
+    this.setState({
+      practice: null,
+      sport: null,
+      day: null,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.displayFilters();
+    let practices = this.state.practice;
+    let sports = this.state.sport;
+    let days = this.state.day;
+    let selection = [];
+    if (practices) selection.practice = practices;
+    if (sports) selection.sport = sports;
     if (days) selection.day = days;
     console.log(typeof selection.pratice);
 
-		this.props.callback(selection);
-	};
+    this.props.callback(selection);
+  };
 
-	render() {
-		if (!this.state.sportsList) return <p>is loading...</p>;
-		else
-			return (
-				<div className="Filter">
-					{!this.state.onDisplay && (
-						<button
-							onClick={this.displayFilters}
-							id="btn-filter-open"
-							className="button"
-							aria-haspopup="true"
-							aria-controls="dropdown-menu"
-						>
-							<span>Filtrer</span>
-							<span className="icon is-small">
-								<i className="fas fa-angle-down" aria-hidden="true"></i>
-							</span>
-						</button>
-					)}
+  render() {
+    if (!this.state.sportsList) return <p>is loading...</p>;
+    else
+      return (
+        <div className="Filter flex">
+          {!this.state.onDisplay && (
+            <button
+              onClick={this.displayFilters}
+              id="btn-filter-open"
+              className="button"
+              aria-haspopup="true"
+              aria-controls="dropdown-menu"
+            >
+              <span>Filtrer</span>
+              <span className="icon is-small">
+                <i className="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          )}
 
-					{this.state.onDisplay && (
-						<form
-							className="filters"
-							onChange={this.handleChange}
-							onSubmit={this.handleSubmit}
-						>
-							<div className="practice control">
-								<label className="radio" htmlFor="filter-competition">
-									<input
-										type="radio"
-										name="practice"
-										id="filter-competition"
-										value="compétition"
-									/>
-									Compétition
-								</label>
-								<label className="radio" htmlFor="filter-loisir">
-									<input
-										type="radio"
-										name="practice"
-										id="filter-loisir"
-										value="loisir"
-									/>
-									Loisir
-								</label>
+          {this.state.onDisplay && (
+            <form
+              className="filters"
+              onChange={this.handleChange}
+              onSubmit={this.handleSubmit}
+            >
+              <div className="practice control">
+                <label className="radio" htmlFor="filter-competition">
+                  <input
+                    type="radio"
+                    name="practice"
+                    id="filter-competition"
+                    value="compétition"
+                  />
+                  Compétition
+                </label>
+                <label className="radio" htmlFor="filter-loisir">
+                  <input
+                    type="radio"
+                    name="practice"
+                    id="filter-loisir"
+                    value="loisir"
+                  />
+                  Loisir
+                </label>
                 {/* FIXME: */}
-								{/* <label className="radio" htmlFor="filter-all">
+                {/* <label className="radio" htmlFor="filter-all">
 									<input
 										type="radio"
 										name="practice"
@@ -103,46 +112,53 @@ class Filter extends React.Component {
 									/>
 									Les 2
 								</label> */}
-							</div>
-							<div className="control">
-								<div className="select">
-									<select name="sport">
-										<option>Sport</option>
-										{this.state.sportsList.map((sport) => (
-											<option key={sport._id} value={sport._id}>
-												{sport.sportName}
-											</option>
-										))}
-									</select>
-								</div>
-							</div>
-							<div className="control">
-								<div className="select">
-									<select name="day">
-										<option>Jour d'entrainement</option>
-										<option>lundi</option>
-										<option>mardi</option>
-										<option>mercredi</option>
-										<option>jeudi</option>
-										<option>vendredi</option>
-										<option>samedi</option>
-										<option>dimanche</option>
-									</select>
-								</div>
-							</div>
-							<div className="control">
-								<button
-									id="btn-filter-apply"
-									className="button is-small is-dark"
-								>
-									Appliquer les filtres
-								</button>
-							</div>
-						</form>
-					)}
-				</div>
-			);
-	}
+              </div>
+              <div className="control">
+                <div className="select">
+                  <select name="sport">
+                    <option>Sport</option>
+                    {this.state.sportsList.map((sport) => (
+                      <option key={sport._id} value={sport._id}>
+                        {sport.sportName}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+              <div className="control">
+                <div className="select">
+                  <select name="day">
+                    <option>Jour d'entrainement</option>
+                    <option>lundi</option>
+                    <option>mardi</option>
+                    <option>mercredi</option>
+                    <option>jeudi</option>
+                    <option>vendredi</option>
+                    <option>samedi</option>
+                    <option>dimanche</option>
+                  </select>
+                </div>
+              </div>
+              <div className="control flex">
+                <button
+                  id="btn-filter-apply"
+                  className="button is-small is-dark"
+                >
+                  Appliquer les filtres
+                </button>
+                <button
+                  type="button"
+                  className="button is-small is-dark delete-filters"
+                  onClick={this.deleteFilters}
+                >
+                  Effacer les filtres
+                </button>
+              </div>
+            </form>
+          )}
+        </div>
+      );
+  }
 }
 
 export default Filter;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,135 +12,143 @@ import Filter from "./../components/Filter";
 import "./../styles/Home.css";
 
 class Home extends Component {
-	state = {
-		teams: [],
-		clubs: [],
-		teamsAndClubs: [],
-		searchValue: "",
-		filteredPractice: null,
-		filteredSport: null,
-		filteredDay: null,
-	};
+  state = {
+    teams: [],
+    clubs: [],
+    teamsAndClubs: [],
+    searchValue: "",
+    filteredPractice: null,
+    filteredSport: null,
+    filteredDay: null,
+  };
 
-	componentDidMount() {
-		Promise.all([teamApiHandler.getTeams(), clubApiHandler.getClubs()])
-			.then((dbRes) => {
-				this.setState({
-					teamsAndClubs: [...dbRes[0], ...dbRes[1]],
-				});
-			})
-			// .then(() =>
-			// 	console.log(
-			// 		"Tous les CLUB + TEAMS de la DB :",
-			// 		this.state.teamsAndClubs
-			// 	)
-			// )
-			.catch((err) => {
-				console.error(err);
-			});
-	}
+  componentDidMount() {
+    Promise.all([teamApiHandler.getTeams(), clubApiHandler.getClubs()])
+      .then((dbRes) => {
+        this.setState({
+          teamsAndClubs: [...dbRes[0], ...dbRes[1]],
+        });
+      })
+      // .then(() =>
+      // 	console.log(
+      // 		"Tous les CLUB + TEAMS de la DB :",
+      // 		this.state.teamsAndClubs
+      // 	)
+      // )
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
-	handleSearch = (place) => {
-		this.setState({ searchValue: place });
-	};
+  handleSearch = (place) => {
+    this.setState({ searchValue: place });
+  };
 
-	handleFilter = (selection) => {
-		// console.log("filter group is there ? ", selection);
-		this.setState({ filteredDay: selection.day });
-		this.setState({ filteredSport: selection.sport });
-		this.setState({ filteredPractice: selection.practice });
-	};
+  handleFilter = (selection) => {
+    // console.log("filter group is there ? ", selection);
+    this.setState({ filteredDay: selection.day });
+    this.setState({ filteredSport: selection.sport });
+    this.setState({ filteredPractice: selection.practice });
+  };
 
-	handleBackground = () => {};
+  handleBackground = () => {};
 
-	render() {
-		const {
-			teamsAndClubs,
-			searchValue,
-			filteredPractice,
-			filteredSport,
-			filteredDay,
-		} = this.state;
+  render() {
+    const {
+      teamsAndClubs,
+      searchValue,
+      filteredPractice,
+      filteredSport,
+      filteredDay,
+    } = this.state;
 
-		// console.log("LE STATE: ", this.state);
-		let closestGroups = [];
+    // console.log("LE STATE: ", this.state);
+    let closestGroups = [];
 
-		if (searchValue) {
-			const lgtSearch = searchValue.geometry.coordinates[0];
-			const latSearch = searchValue.geometry.coordinates[1];
-			const to = turf.point([lgtSearch, latSearch]);
+    if (searchValue) {
+      const lgtSearch = searchValue.geometry.coordinates[0];
+      const latSearch = searchValue.geometry.coordinates[1];
+      const to = turf.point([lgtSearch, latSearch]);
 
-			teamsAndClubs.forEach((elem) => {
-				let lgtElem = elem.location.coordinates[0];
-				let latElem = elem.location.coordinates[1];
-				let from = turf.point([lgtElem, latElem]);
-				let options = { units: "kilometers" };
-				let distance = turf.distance(from, to, options);
-				if (distance <= 20) closestGroups.push(elem);
-			});
-		}
-		// console.log("elem in less than 20 km : ", closestGroups);
+      teamsAndClubs.forEach((elem) => {
+        let lgtElem = elem.location.coordinates[0];
+        let latElem = elem.location.coordinates[1];
+        let from = turf.point([lgtElem, latElem]);
+        let options = { units: "kilometers" };
+        let distance = turf.distance(from, to, options);
+        if (distance <= 20) closestGroups.push(elem);
+      });
+    }
+    // console.log("elem in less than 20 km : ", closestGroups);
 
-		// TODO: AJOUTER LE FILTRE DES JOURS (nesté !)
-		// TODO: attention au group.sport dont il faudra peut-être retrouver l'id quand le populate fonctionnera !
-		const results = closestGroups
-			.filter((group) => {
-				if ((!filteredDay, !filteredPractice, !filteredSport)) return true;
-				if (filteredSport) return group.sport === filteredSport;
-			})
-			.filter((group) => {
-				if (filteredPractice) return group.practice === filteredPractice;
-				else return true;
-			})
-			.filter((group) => {
-				if (filteredDay) return group.trainings.day.includes(filteredDay);
-				else return true;
-			});
+    // TODO: AJOUTER LE FILTRE DES JOURS (nesté !)
+    // TODO: attention au group.sport dont il faudra peut-être retrouver l'id quand le populate fonctionnera !
+    const results = closestGroups
+      .filter((group) => {
+        console.log("===============group", group);
+        if ((!filteredDay, !filteredPractice, !filteredSport)) return true;
+        if (filteredSport && group.sport)
+          return group.sport._id === filteredSport;
+      })
+      .filter((group) => {
+        if (filteredPractice) return group.practice === filteredPractice;
+        else return true;
+      })
+      .filter((group) => {
+        if (filteredDay && group.trainings) {
+          return group.trainings.filter((training) => {
+            console.log("===============training", training);
+            return training.day === filteredDay;
+          });
+        } else {
+          return true;
+        }
+      });
 
-		console.log("filtered groups ", results);
-		// console.log("search value ", results);
+    console.log("filtered groups ", results);
+    // console.log("search value ", results);
 
-		return (
-			<div className="ContentMain">
-				{/* SEARCHBAR + FILTERS--------------------- */}
-				<div className="flex space-btw">
-					<SearchBar callback={this.handleSearch} />
-					{searchValue && <Filter callback={this.handleFilter} />}
-				</div>
-				{/* BACKGROUND  */}
-				<div className={!searchValue ? "bg-main" : "hidden"}>
-					{/* DISPLAYED CARDS ----------------------------*/}
-					<div className="cards-container grid fr-5">
-						{results.map((group, index) => (
-							<Card key={index} index={index} elem={group} />
-						))}
-						{!results && <li>Loading...</li>}
-					</div>
+    return (
+      <div className="ContentMain">
+        {/* SEARCHBAR + FILTERS--------------------- */}
+        <div className="flex space-btw">
+          <SearchBar callback={this.handleSearch} />
+          {searchValue && <Filter callback={this.handleFilter} />}
+        </div>
+        {/* BACKGROUND  */}
+        <div className={!searchValue ? "bg-main" : "hidden"}>
+          {/* DISPLAYED CARDS ----------------------------*/}
+          <div className="cards-container grid fr-5">
+            {results.map((group, index) => (
+              <Card key={index} index={index} elem={group} />
+            ))}
+            {!results && <li>Loading...</li>}
+          </div>
 
-					{results.length === 0 && searchValue && (
-						<p className="topo">
-							Pas de clubs à l'horizon, veuillez modifier votre recherche.
-						</p>
-					)}
+          {results.length === 0 && searchValue && (
+            <p className="topo">
+              Pas de clubs à l'horizon, veuillez modifier votre recherche.
+            </p>
+          )}
 
-					{/* DISPLAYED CONTENT BEFORE SEARCH -------------- */}
-					{!searchValue && (
-						<div className="presentation-content bordered-round flex col">
-							<div className="title-container">
-								<h1 className="title">
-									Tous les sports pour toutes les femmes, partout !
-								</h1>
-								<h2 className="subtitle">
-									Clubs amateurs pour sportives tous niveaux, sports collectifs
-									ou individuels en club
-								</h2>
-								<div className="topo">Women players only</div>
-							</div>
-						</div>
-					)}
-				</div>
-			</div>
-		);
-	}
+          {/* DISPLAYED CONTENT BEFORE SEARCH -------------- */}
+          {!searchValue && (
+            <div className="presentation-content bordered-round flex col">
+              <div className="title-container">
+                <h1 className="title">
+                  Tous les sports pour toutes les femmes, partout !
+                </h1>
+                <h2 className="subtitle">
+                  Clubs amateurs pour sportives tous niveaux, sports collectifs
+                  ou individuels en club
+                </h2>
+                <div className="topo">Women players only</div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 export default withRouter(Home);

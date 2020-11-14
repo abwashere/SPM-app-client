470bda64d94cf225576e2ff08a5c6a920c5b7529
abWasHere <audrey.belson@gmail.com>
2020-11-14 11:47:47 +0100

    Add button to access Profile on Account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -9,120 +9,135 @@ import clubApiHandler from "./../api/clubApiHandler";
 import "./../styles/Account.css";
 
 class Account extends React.Component {
-  static contextType = UserContext;
+	static contextType = UserContext;
 
-  state = { displayDelete: false, clubTeams: [], clubEvents: [] };
+	state = { displayDelete: false, clubTeams: [], clubEvents: [] };
 
-  componentDidMount() {
-    let user = this.context.user;
+	componentDidMount() {
+		let user = this.context.user;
 
-    clubApiHandler
-      .getTeamsOfClub(user._id)
-      .then((dbResTeams) => {
-        // console.log("teams du club:", dbResTeams);
-        this.setState({ clubTeams: dbResTeams });
-      })
-      .catch((err) => console.log(err))
-      .then(() => {
-        clubApiHandler
-          .getEventsOfClub(user._id)
-          .then((dbResEvents) => {
-            // console.log("events du club:", dbResEvents);
-            this.setState({ clubEvents: dbResEvents });
-          })
-          .catch((err) => console.log(err));
-      });
-  }
+		clubApiHandler
+			.getTeamsOfClub(user._id)
+			.then((dbResTeams) => {
+				// console.log("teams du club:", dbResTeams);
+				this.setState({ clubTeams: dbResTeams });
+			})
+			.catch((err) => console.log(err))
+			.then(() => {
+				clubApiHandler
+					.getEventsOfClub(user._id)
+					.then((dbResEvents) => {
+						// console.log("events du club:", dbResEvents);
+						this.setState({ clubEvents: dbResEvents });
+					})
+					.catch((err) => console.log(err));
+			});
+	}
 
-  handleDisplayDelete = () => {
-    this.setState({ displayDelete: !this.state.displayDelete });
-  };
+	handleDisplayDelete = () => {
+		this.setState({ displayDelete: !this.state.displayDelete });
+	};
 
-  render() {
-    const { displayDelete, clubTeams, clubEvents } = this.state;
-    let role = this.context.user.role;
+	render() {
+		const { displayDelete, clubTeams, clubEvents } = this.state;
+		let role = this.context.user.role;
 
-    return (
-      <div className="Account ContentMain">
-        <h1 className="title">Mon compte</h1>
+		return (
+			<div className="Account ContentMain">
+				<h1 className="title">Mon compte</h1>
 
-        <div className="forms flex space-btw ">
-          {/*--------------------------- SECTION MODIF INFOS PERSOS */}
-          <div className="edit-left col">
-            {/*-------------------- edit section --------- */}
-            <FormEditAccount />
-            {/* ------------------delete section --------- */}
-            <div className="edit-bottom">
-              <button
-                onClick={this.handleDisplayDelete}
-                className="button is-danger is-outlined"
-              >
-                Supprimer mon compte
-              </button>
-              {displayDelete && (
-                <FormDeleteAccount
-                  role={this.context.user.role}
-                  abortDelete={this.handleDisplayDelete}
-                />
-              )}
-            </div>
-          </div>
+				<div className="forms flex space-btw ">
+					{/*--------------------------- SECTION MODIF INFOS PERSOS */}
+					<div className="edit-left col">
+						{/*-------------------- edit section --------- */}
+						<FormEditAccount />
+						{/*-------------------- profile link section --------- */}
+						{role === "Club" ? (
+							<button type="button" className="button is-outlined">
+								<Link to={`/club/${this.context.user._id}`}>
+									Voir le profile du Club
+								</Link>
+							</button>
+						) : (
+							<button type="button" className="button is-outlined">
+								<Link to={`/player/${this.context.user._id}`}>
+									Voir mon profile
+								</Link>
+							</button>
+						)}
 
-          {/*-------------- SECTION MODIF TEAMS ET EVENTS pour clubs */}
-          {role === "Club" && (
-            <div className="edit-right">
-              {/* ajouter des liens sur les noms */}
-              <p className="subtitle">Mettre à jour mes équipes</p>
-              {clubTeams.length === 0 ? (
-                <p>Aucun évènement créé.</p>
-              ) : (
-                <div className="teams">
-                  <ul>
-                    {clubTeams.map((team) => (
-                      <li key={team._id}>
-                        <Link
-                          className="link hvr-underline-from-center"
-                          to={`/account/team/edit/${team._id}`}
-                        >
-                          {team.teamName}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-              <button className="button is-success hvr-pulse-grow">
-                <Link to={`/account/team/create`}>Ajouter une équipe</Link>
-              </button>
+						{/* ------------------delete section --------- */}
+						<div className="edit-bottom">
+							<button
+								onClick={this.handleDisplayDelete}
+								className="button is-danger is-outlined"
+							>
+								Supprimer mon compte
+							</button>
+							{displayDelete && (
+								<FormDeleteAccount
+									role={this.context.user.role}
+									abortDelete={this.handleDisplayDelete}
+								/>
+							)}
+						</div>
+					</div>
 
-              <p className="subtitle">Mettre à jour mes évènements</p>
-              {clubEvents.length === 0 ? (
-                <p>Aucun évènement créé.</p>
-              ) : (
-                <div className="events">
-                  <ul>
-                    {clubEvents.map((event) => (
-                      <li key={event._id}>
-                        <Link
-                          className="link hvr-underline-from-center"
-                          to={`/account/event/edit/${event._id}`}
-                        >
-                          {event.title}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-              <button className="button is-success hvr-pulse-grow">
-                <Link to={`/account/event/create`}>Ajouter un évènement</Link>
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-    );
-  }
+					{/*-------------- SECTION MODIF TEAMS ET EVENTS pour clubs */}
+					{role === "Club" && (
+						<div className="edit-right">
+							{/* ajouter des liens sur les noms */}
+							<p className="subtitle">Mettre à jour mes équipes</p>
+							{clubTeams.length === 0 ? (
+								<p>Aucun évènement créé.</p>
+							) : (
+								<div className="teams">
+									<ul>
+										{clubTeams.map((team) => (
+											<li key={team._id}>
+												<Link
+													className="link hvr-underline-from-center"
+													to={`/account/team/edit/${team._id}`}
+												>
+													{team.teamName}
+												</Link>
+											</li>
+										))}
+									</ul>
+								</div>
+							)}
+							<button className="button is-success hvr-pulse-grow">
+								<Link to={`/account/team/create`}>Ajouter une équipe</Link>
+							</button>
+
+							<p className="subtitle">Mettre à jour mes évènements</p>
+							{clubEvents.length === 0 ? (
+								<p>Aucun évènement créé.</p>
+							) : (
+								<div className="events">
+									<ul>
+										{clubEvents.map((event) => (
+											<li key={event._id}>
+												<Link
+													className="link hvr-underline-from-center"
+													to={`/account/event/edit/${event._id}`}
+												>
+													{event.title}
+												</Link>
+											</li>
+										))}
+									</ul>
+								</div>
+							)}
+							<button className="button is-success hvr-pulse-grow">
+								<Link to={`/account/event/create`}>Ajouter un évènement</Link>
+							</button>
+						</div>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Account;
